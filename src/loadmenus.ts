156d59fa75408f05a638f2005db262d296269f2a
James Madden <jamesbmadden@gmail.com>
2022-09-20 20:46:42 -0400

    Load the text content of the new res menu onto the page!

---
+++ src/loadmenus.ts
@@ -0,0 +1,50 @@
+/**
+ * This is a series of functions that provide the infrastructure for loading the dining hall menus by making use of 
+ * puppeteer to load the website, grab the text content of the PDF files, and output it.
+ */
+import puppeteer, { HTTPRequest } from 'puppeteer';
+import getTextFromPdf from './pdf';
+
+export default async function loadMenus () {
+
+  // load a browser instance
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const nrh = await loadMenuText(page, 'nrh');
+
+  // make sure to shut down the browser
+  await browser.close();
+
+  return nrh;
+
+}
+
+/**
+ * Open the pdf for the dining hall's menu, and copy all the text for further processing
+ * 
+ * @param res The code included in the url for the dining hall we're looking at
+ * @returns A promise resolving to the text content of the pdf
+ */
+export async function loadMenuText (page: puppeteer.Page, res: string): Promise<string> {
+ 
+   // navigate to the webstie
+   await page.goto('https://www.mcgill.ca/foodservices/locations/dining-hall-menus');
+ 
+   // grab the href attribute for the button so we can fetch that pdf
+   const url = await page.$eval(`.button--outline[href*=${res}]`, element => element.getAttribute('href'));
+
+
+   // if url is null, throw
+   if (url === null) throw "Couldn't get the URL for " + res;
+ 
+   // download the pdf using the node fetch api
+   // @ts-ignore
+   const pdf = await (await fetch(new URL("https:" + url))).blob();
+
+   // use pdfjs to read the text
+ 
+   // return the image to the user
+   return getTextFromPdf(Buffer.from(await pdf.arrayBuffer()));
+ 
+ }
---
+++ src/pdf.ts
@@ -0,0 +1,21 @@
+import { getDocument } from 'pdfjs-dist';
+import type { TextItem } from 'pdfjs-dist/types/src/display/api';
+
+export default async function getTextFromPdf (source: Buffer): Promise<string> {
+
+  // load the pdf file from the source provided
+  const pdf = await getDocument(source).promise;
+
+  // all the pages have to be loaded and then their text grabbed
+  // Basically, wait for all the promises loading the pages to be resolved.
+  const pages = await Promise.all(Array.from({ length: pdf.numPages }, (_, i) => pdf.getPage(i + 1)));
+
+  // loop over all the pages and grab their text content
+  const pageText = await Promise.all(pages.map(page => page.getTextContent()));
+
+  // finally, we can join all of the text together and return it. 
+  // Because pageText is made up of TextContents, not actually just strings, we need to map stuff out
+  // @ts-ignore
+  return pageText.map(({ items }) => items.map(({ str }) => str).join("")).join("");
+
+}
